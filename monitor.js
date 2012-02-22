22b21ffc084ababcdfb10ebe2754c71b51bfc01b
Maxime Vedelago <maxime.vedelago@buglabs.net>
2012-02-22 17:10:43 -0500

    CSS and HTML changes. Removed the logs

--- monitor.js
+++ monitor.js
@@ -36,25 +36,6 @@ errorHandler = function(response)
 updateResources = function(response) 
 {
 	presence = JSON.parse(response).presence;
-	// Filter out swarm presence messages, which do not contain the 'type' field
-	if(presence.type) 
-	{
-		var who = presence.from.resource;
-		var alive = (presence.type === "available");
-
-		//if somebody joins or quits the chat, update the chatters list
-		if (alive) 
-		{
-			$('#resources').append("<li id='" + who + "'>" + who + ' is now conneted.</li>');	
-		}
-		else 
-		{			
-			$('#resources').append("<li id='" + who + "'>" + who + ' just left.</li>');
-		}
-		
-		//Get the log and scroll down, so that the last log could be seen
-		document.querySelector('#resources').scrollTop = document.querySelector('#resources').scrollHeight;
-	}
 };
 
 
