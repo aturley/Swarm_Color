9291b102387293ef9e85eaedb5c2b6f7268650ec
Maxime Vedelago <maxime.vedelago@buglabs.net>
2012-02-23 11:52:25 -0500

    Creation of a script that generates automatically the monitor array.

---
+++ monitor_table.js
@@ -0,0 +1,46 @@
+//Creation of the table
+var mainTable = document.createElement('table');
+mainTable.id = 'monitorTable';
+
+//Creation of the columns
+var column = [];
+var numberColumns = 10;
+for (var i = 0; i < numberColumns; i++)
+{
+	var columnElement = document.createElement('tr');
+	columnElement.id = '__' + i;
+	column.push(columnElement);
+}
+
+//Creation of the rows
+var numberRows = 20;
+//Here, I create a matrix, which will contain all the rows we need
+var row = new Array(numberColumns); for ( var i = 0; i < numberRows; i++) { row[i] = new Array(); }
+//Loop which create the "td"s tags
+for (var i = 0; i < numberRows; i++)
+{
+	for (var j = 0; j < numberColumns; j++)
+	{
+		var rowElement = document.createElement('td');
+		rowElement.className = '_' + i;
+		row[j].push(rowElement);
+	}
+}
+
+//Insertion of the "td"s in the "tr"s
+for (var i = 0; i < numberColumns; i++)
+{
+	for(var j = 0; j < numberRows; j++)
+	{
+		column[i].appendChild(row[i][j]);
+	}
+}
+
+//Insertion of the "tr"s in the table
+for (var i = 0; i < numberColumns; i++)
+{
+	mainTable.appendChild(column[i]);
+}
+
+//Insertion of mainTable in the <body>
+document.body.appendChild(mainTable);
